5c85729e5434a00896de84eb4e4c96efaaa34ab5
Hector <hector@ejemplo.com>
2020-07-07 18:19:12 -0400

    ad12

--- hostalapp/views.py
+++ hostalapp/views.py
@@ -772,7 +772,10 @@ def UpdateProveedor (request):
 def OrdenDePedidos(request):
     ordenPedido = HOrdenPedido.objects.all()
     print(ordenPedido)
-    return render(request, 'hostal/OrdenDePedidos.html',{'ordenPedido':ordenPedido})
+    form= {  
+        "ayuda" : ayuda[3]
+        }
+    return render(request, 'hostal/OrdenDePedidos.html',{'ordenPedido':ordenPedido, "form": form})
 
 
 def mainHostal(request):
