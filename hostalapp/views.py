ffb3817ff891c3d9a024977878476c163a051222
sergio <ser.alvarezm@alumnos.duoc.cl>
2020-07-07 18:32:22 -0400

    cambios

--- hostalapp/views.py
+++ hostalapp/views.py
@@ -285,106 +285,104 @@ def CrearNuevoCliente(request):
 
 def GuardarNuevoCliente(request):
 
-    cliente = HOrganismo()
+    username = request.POST["username"]
+    rutEmpresa = request.POST["rol_empresa"]
 
-    usuario = HUsuario()
+    # VALIDANDO NOMRBE DE USUARIO
+    if HUsuario.objects.filter(username = username).count() > 0:
 
+        messages.error(request, "El nombre de usuario utilizado ya se encuentra en uso.")
+
+        region = []
+        for r in HRegion.objects.all():
+            region.append(r)
+        form = {
+            "datos" : request.POST,
+            "region" : region
+        }
+        return render(request, "hostal/CrearNuevoCliente.html",{'form':form, 'nav':'/AdminClientesAgregar/'})
+
+    # VALIDANDO RUT DEL ORGANISMO
+    if HOrganismo.objects.filter(rut = rutEmpresa).count()>0:
+
+        messages.error(request, "El rol inRol de empresa ya se encuentra registrado.")
+        region = []
+        for r in HRegion.objects.all():
+            region.append(r)
+        form = {
+            'datos':request.POST,
+            "region" : region
+        }
+        return render(request, "hostal/CrearNuevoCliente.html", {'form':form, 'nav':'/AdminClientesAgregar/'})
+
+    # PERSONA
     persona = HPersona(
         persona_id = getSecuenciaId("H_PERSONA_PERSONA_ID_SEQ"),
         nombres = request.POST["nombre_persona"],
         paterno = request.POST["Ap_paterno"],
         materno = request.POST["Ap_materno"]
     )
+    persona.save()
 
+    # DIRECCION
+    direccionP = HPersonaDireccion(
+        persona_direccion_id = getSecuenciaId ("H_PERSONA_DIRECCION_PERSONA_DI"),
+        telefono = request.POST["Ptelefono"],
+        email =request.POST["Pemail"],
+        persona = persona,
+        usuario = usuarioActual(), # 56 - Usuario en sesión
+        registro_fecha = datetime.now(),
+        registro_hora = (now.hour*100)+now.minute
+    )
+    direccionP.save()
 
-    usuario.persona_id=persona.persona_id
+    # USUARIO
+    perfil=HUsuarioPerfil.objects.get(usuario_perfil_id=4)
+    usuario = HUsuario(
+        usuario_id = getSecuenciaId ("H_USUARIO_USUARIO_ID_SEQ"),
+        persona=persona,
+        username = request.POST["username"],
+        contrasena = encode(WORDFISH, request.POST["contrasena"]),
+        registro_fecha = datetime.now(),
+        usuario_perfil = perfil,
+        vigencia = 1
+    )
+    usuario.save()
 
-    usuario.usuario_id=getSecuenciaId("H_PERSONA_PERSONA_ID_SEQ")
-    usuario.username=request.POST["username"]
+    comuna=HComuna.objects.get(comuna_id=request.POST["comunaId"])
 
-    #Direccion Usuario
+    # ORGANISMO
+    organismo = HOrganismo(
+        organismo_id = getSecuenciaId ("H_ORGANISMO_ORGANISMO_ID_SEQ"),
+        razon_social = request.POST["razon_social"],
+        rut = request.POST["rol_empresa"],
+        nombre_fantasia = request.POST["nombre_fantasia"],
+        giro = "", #request.POST["giro"],
+        direccion = request.POST["direccion"],
+        telefono = request.POST["telefono"],
+        cuenta_datos = request.POST["cuenta"],
+        persona = persona,
+        usuario = usuarioActual(),
+        registro_fecha = datetime.now(),
+        comuna = comuna,
+        vigencia = 1
+    )
+    organismo.save()
 
-
-
-
-    cliente.usuario_id = usuario.usuario_id
-    cliente.persona_id = usuario.persona_id
-    cliente.razon_social = request.POST["razon_social"]
-
-    cliente.rut = request.POST["rol_empresa"]
-
-
-
-
-    cliente.nombre_fantasia = request.POST["nombre_empresa"]
-
-    if HOrganismo.objects.filter(rut = cliente.rut).count()>0:
-        messages.error(request, "Rol de empresa ya se encuentra registrado.")
-
-        form = {
-        'persona':persona,
-        'usuario':usuario,
-        'cliente':cliente
-        }
-
-        return render(request, "hostal/CrearNuevoCliente.html",{'form':form})
-
-
-    elif HOrganismo.objects.filter(nombre_fantasia=cliente.nombre_fantasia).count()>0:
-        messages.error(request, "Nombre de empresa ya se encuentra registrado.")
-
-        form = {
-        'persona':persona,
-        'usuario':usuario,
-        'cliente':cliente
-        }
-
-        return render(request, "hostal/CrearNuevoCliente.html",{'form':form})
-    elif HUsuario.objects.filter(username= usuario.username).count() > 0:
-        messages.error(request, "Nombre de Usuario ya existe.")
-        print("Usuario ", usuario.username, " ya existe")
-
-        form = {
-        'persona':persona,
-        'usuario':usuario,
-        'cliente':cliente
-        }
-
-        return render(request, "hostal/CrearNuevoCliente.html",{'form':form})
-
-    else:
-
-
-        persona.save()
-        print("Persona "+str(persona.persona_id))
-        usuario.contrasena=encode(WORDFISH, request.POST["contrasena"])
-        usuario.vigencia=1
-        perfil = HUsuarioPerfil.objects.get(usuario_perfil_id=3)
-
-        usuario.usuario_perfil_id=perfil.usuario_perfil_id
-
-        usuario.save()
-
-        cliente.direccion = request.POST["direccion"]
-        cliente.telefono = request.POST["telefono"]
-
-        comuna = HComuna.objects.get(comuna_id=2)
-        cliente.comuna_id=comuna.comuna_id
-        cliente.vigencia=1
-
-        try:
-            direccionP.save()
-
-            cliente.save()
-            messages.success(request, 'Registro Exitoso.')
-        except Exception as e:
-            messages.error(request, 'Ocurrió un error en el Registro.')
-    #return HttpResponseRedirect('/GuardarNuevoProvedor/AdminProveedor')
-    cl=HOrganismo.objects.all()
     form = {
-    'cl':cl}
-    return render(request, "hostal/CrearNuevoCliente.html",{'form':form})
+        "msg":"Cliente creado exitosamente.",
+    }
 
+    try:
+        cliente = HOrganismo.objects.distinct(proveedor_flag =1)
+    except:
+        cliente = { }
+
+    form = {
+            'cliente' : cliente
+        }
+
+    return render(request, "hostal/AdminClientesAgregar.html", { 'form':form, 'nav':'/mainHostal/' })
 
 def CrearNuevoProovedor(request):
 
